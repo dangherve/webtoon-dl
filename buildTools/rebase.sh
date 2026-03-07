24fe26021a53fd276d97a8f0eb602a030a5c3af5
dangherve <dangherve@gmail.com>
2026-03-07 01:20:12 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/robinovitch61/webtoon-dl.git
+git fetch upstream
+git rebase upstream/main
+git remote set-url origin git@github.com:dangherve/webtoon-dl.git
+git push --force --set-upstream origin master
